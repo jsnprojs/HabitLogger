4974a711d86f14a92e874a3728fa665fabd03b10
jsnprojs <132707597+jsnprojs@users.noreply.github.com>
2024-03-27 20:25:45 +0800

    Create simple sql table

---
+++ HabitLogger/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitLogger;
+
+internal class Program
+{
+    static string connectionString = @"Data Source=habit-Tracker.db";
+    static void Main(string[] args)
+    {
+        using (var connection = new SqliteConnection(connectionString))
+        {
+
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+
+            tableCmd.CommandText =
+                @"CREATE TABLE IF NOT EXISTS drinking_water (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date TEXT,
+                        Quantity INTEGER
+                        )";
+
+            tableCmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
