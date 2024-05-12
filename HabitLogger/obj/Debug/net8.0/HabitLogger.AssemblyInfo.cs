8c3b8c93f2350ca29adc8f3d3aa28d83c304393f
kang yi fan <jason3631061@gmail.com>
2024-05-12 15:37:53 -0700

    Add GenerateDefaultHabits when there is no rows in the habits table, fix GenerateRandomRecords, it will now generate date correctly

--- HabitLogger/obj/Debug/net8.0/HabitLogger.AssemblyInfo.cs
+++ HabitLogger/obj/Debug/net8.0/HabitLogger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6216b5aa5ba7bdbbc6915185f7be08e451f9e54f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26bdf1d1dcf5caa6a3a9542ab7399d5d2cfb976f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
